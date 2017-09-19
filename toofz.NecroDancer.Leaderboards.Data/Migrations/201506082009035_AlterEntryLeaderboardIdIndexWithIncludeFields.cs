02acacca698f851a6af96646e09a7c4c96502f7f
Leonard Thieu <leonard.thieu@gmail.com>
2017-09-19 14:41:44 -0400

    Renam project and drop a namespace level.

---
+++ toofz.NecroDancer.Leaderboards.Data/Migrations/201506082009035_AlterEntryLeaderboardIdIndexWithIncludeFields.cs
@@ -0,0 +1,17 @@
+namespace toofz.NecroDancer.Leaderboards.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AlterEntryLeaderboardIdIndexWithIncludeFields : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
