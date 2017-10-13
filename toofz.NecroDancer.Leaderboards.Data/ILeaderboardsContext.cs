0a9dcba2824ef167b774e3b8ef6cb7c5b99ee34e
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-13 10:26:16 -0400

    Make DbSet properties read-only.

--- toofz.NecroDancer.Leaderboards.Data/ILeaderboardsContext.cs
+++ toofz.NecroDancer.Leaderboards.Data/ILeaderboardsContext.cs
@@ -5,15 +5,15 @@ namespace toofz.NecroDancer.Leaderboards
 {
     public interface ILeaderboardsContext : IDisposable
     {
-        DbSet<Leaderboard> Leaderboards { get; set; }
-        DbSet<Entry> Entries { get; set; }
-        DbSet<DailyLeaderboard> DailyLeaderboards { get; set; }
-        DbSet<DailyEntry> DailyEntries { get; set; }
-        DbSet<Player> Players { get; set; }
-        DbSet<Replay> Replays { get; set; }
-        DbSet<Product> Products { get; set; }
-        DbSet<Mode> Modes { get; set; }
-        DbSet<Run> Runs { get; set; }
-        DbSet<Character> Characters { get; set; }
+        DbSet<Leaderboard> Leaderboards { get; }
+        DbSet<Entry> Entries { get; }
+        DbSet<DailyLeaderboard> DailyLeaderboards { get; }
+        DbSet<DailyEntry> DailyEntries { get; }
+        DbSet<Player> Players { get; }
+        DbSet<Replay> Replays { get; }
+        DbSet<Product> Products { get; }
+        DbSet<Mode> Modes { get; }
+        DbSet<Run> Runs { get; }
+        DbSet<Character> Characters { get; }
     }
 }
