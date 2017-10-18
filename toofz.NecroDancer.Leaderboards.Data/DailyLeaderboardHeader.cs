4c150a74f5b4d776f2af54c55d50d43709cf4783
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-18 08:12:13 -0400

    Add LeaderboardResources.

---
+++ toofz.NecroDancer.Leaderboards.Data/DailyLeaderboardHeader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace toofz.NecroDancer.Leaderboards
+{
+    [DataContract]
+    public sealed class DailyLeaderboardHeader
+    {
+        [DataMember(Name = "id", IsRequired = true)]
+        public int Id { get; set; }
+        [DataMember(Name = "product", IsRequired = true)]
+        public string Product { get; set; }
+        [DataMember(Name = "date", IsRequired = true)]
+        public DateTime Date { get; set; }
+        [DataMember(Name = "production")]
+        public bool IsProduction { get; set; }
+    }
+}
