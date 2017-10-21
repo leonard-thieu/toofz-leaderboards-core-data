7e465476e6215d8a8a3021da9b928e9412263785
Leonard Thieu <leonard-thieu@users.noreply.github.com>
2017-10-21 11:27:13 -0400

    Add LeaderboardResources (#3)

---
+++ toofz.NecroDancer.Leaderboards.Data/LeaderboardHeader.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace toofz.NecroDancer.Leaderboards
+{
+    [DataContract]
+    public sealed class LeaderboardHeader
+    {
+        [DataMember(Name = "id", IsRequired = true)]
+        public int Id { get; set; }
+        [DataMember(Name = "display_name", IsRequired = true)]
+        public string DisplayName { get; set; }
+        [DataMember(Name = "product", IsRequired = true)]
+        public string Product { get; set; }
+        [DataMember(Name = "mode", IsRequired = true)]
+        public string Mode { get; set; }
+        [DataMember(Name = "run", IsRequired = true)]
+        public string Run { get; set; }
+        [DataMember(Name = "character", IsRequired = true)]
+        public string Character { get; set; }
+        [DataMember(Name = "production")]
+        public bool IsProduction { get; set; }
+        [DataMember(Name = "custom_music")]
+        public bool IsCustomMusic { get; set; }
+        [DataMember(Name = "co_op")]
+        public bool IsCoOp { get; set; }
+    }
+}
