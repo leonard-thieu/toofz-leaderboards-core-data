77c8889cb7918a03ff46c95a620184dce3c1cfdc
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-22 13:34:03 -0400

    Fix issues with generating leaderboard names in migration. * `All Chars` and `All Chars DLC` were switched. * Add support for Co-op leaderboards. * Add support for Custom Music leaderboards. * Add support for Classic Early Access leaderboards.

--- toofz.NecroDancer.Leaderboards.Data/Migrations/201710221313149_WhiteBrassPointer.cs
+++ toofz.NecroDancer.Leaderboards.Data/Migrations/201710221313149_WhiteBrassPointer.cs
@@ -30,53 +30,65 @@ WHERE ProductId = 1;");
 
             AddColumn("dbo.Leaderboards", "Name", c => c.String(nullable: false));
             Sql(Util.DropDefaultConstraint("dbo.Leaderboards", "Name"));
-            // This doesn't handle Classic Early Access, Custom Music, or Co-op leaderboards.
-            // While the schema supports it, leaderboards of those types did not exist in the production database.
             Sql($@"UPDATE dbo.Leaderboards
-SET Name = RTRIM(CASE ProductId
-               WHEN 1 THEN 'DLC '
-               ELSE ''
-           END + 
-           CASE RunId
-               WHEN 0 THEN 'HARDCORE '
-               WHEN 1 THEN 'SPEEDRUN '
-               WHEN 2 THEN 'HARDCORE SEEDED '
-               WHEN 3 THEN 'SEEDED SPEEDRUN '
-               WHEN 4 THEN 'HARDCORE '
-               ELSE ''
-           END + 
-           CASE CharacterId
-               WHEN -4 THEN 'All Chars '
-               WHEN -3 THEN 'All Chars DLC '
-               WHEN -2 THEN 'Story Mode '
-               WHEN 0 THEN ''
-               WHEN 1 THEN 'Melody '
-               WHEN 2 THEN 'Aria '
-               WHEN 3 THEN 'Dorian '
-               WHEN 4 THEN 'Eli '
-               WHEN 5 THEN 'Monk '
-               WHEN 6 THEN 'DOVE '
-               WHEN 7 THEN 'Coda '
-               WHEN 8 THEN 'Bolt '
-               WHEN 9 THEN 'Bard '
-               WHEN 10 THEN 'Nocturna '
-               WHEN 11 THEN 'Diamond '
-               WHEN 12 THEN 'Mary '
-               WHEN 13 THEN 'Tempo '
-               ELSE ''
-           END + 
-           CASE RunId
-               WHEN 4 THEN 'DEATHLESS '
-               ELSE ''
-           END +
-           CASE ModeId
-               WHEN 1 THEN 'NO RETURN'
-               WHEN 2 THEN 'HARD'
-               WHEN 3 THEN 'PHASING'
-               WHEN 4 THEN 'RANDOMIZER'
-               WHEN 5 THEN 'MYSTERY'
-               ELSE ''
-           END) + '_PROD'");
+SET Name = RTRIM(
+    CASE ProductId
+        WHEN 1 THEN 'DLC '
+        ELSE ''
+    END + 
+    CASE RunId
+        WHEN 0 THEN 'HARDCORE '
+        WHEN 1 THEN 'SPEEDRUN '
+        WHEN 2 THEN 'HARDCORE SEEDED '
+        WHEN 3 THEN 'SEEDED SPEEDRUN '
+        WHEN 4 THEN 'HARDCORE '
+        ELSE ''
+    END + 
+    CASE CharacterId
+        WHEN -4 THEN 'All Chars DLC '
+        WHEN -3 THEN 'All Chars '
+        WHEN -2 THEN 'Story Mode '
+        WHEN 0 THEN ''
+        WHEN 1 THEN 'Melody '
+        WHEN 2 THEN 'Aria '
+        WHEN 3 THEN 'Dorian '
+        WHEN 4 THEN 'Eli '
+        WHEN 5 THEN 'Monk '
+        WHEN 6 THEN 'DOVE '
+        WHEN 7 THEN 'Coda '
+        WHEN 8 THEN 'Bolt '
+        WHEN 9 THEN 'Bard '
+        WHEN 10 THEN 'Nocturna '
+        WHEN 11 THEN 'Diamond '
+        WHEN 12 THEN 'Mary '
+        WHEN 13 THEN 'Tempo '
+        ELSE ''
+    END + 
+    CASE IsCoOp
+        WHEN 1 THEN 'CO-OP '
+        ELSE ''
+    END + 
+    CASE RunId
+        WHEN 4 THEN 'DEATHLESS '
+        ELSE ''
+    END +
+    CASE ModeId
+        WHEN 1 THEN 'NO RETURN '
+        WHEN 2 THEN 'HARD '
+        WHEN 3 THEN 'PHASING '
+        WHEN 4 THEN 'RANDOMIZER '
+        WHEN 5 THEN 'MYSTERY '
+        ELSE ''
+    END + 
+    CASE IsCustomMusic
+        WHEN 1 THEN 'CUSTOM MUSIC '
+        ELSE ''
+    END
+) + 
+CASE
+    WHEN ProductId <> 0 OR IsProduction <> 0 THEN '_PROD'
+    ELSE ''
+END");
         }
 
         public override void Down()
