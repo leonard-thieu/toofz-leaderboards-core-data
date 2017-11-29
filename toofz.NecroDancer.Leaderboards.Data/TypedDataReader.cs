dbca528304ebd6838d7881717e6e8b235f165404
Leonard Thieu <leonard.thieu@gmail.com>
2017-11-29 11:05:54 -0500

    Exclude from code coverage.

--- toofz.NecroDancer.Leaderboards.Data/TypedDataReader.cs
+++ toofz.NecroDancer.Leaderboards.Data/TypedDataReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity.Core.Mapping;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -52,35 +53,35 @@ namespace toofz.NecroDancer.Leaderboards
 
         #region Not used by SqlBulkCopy (satisfying interface only)
 
-        string IDataRecord.GetName(int i) => throw new NotImplementedException();
-        string IDataRecord.GetDataTypeName(int i) => throw new NotImplementedException();
-        Type IDataRecord.GetFieldType(int i) => throw new NotImplementedException();
-        int IDataRecord.GetValues(object[] values) => throw new NotImplementedException();
-        bool IDataRecord.GetBoolean(int i) => throw new NotImplementedException();
-        byte IDataRecord.GetByte(int i) => throw new NotImplementedException();
-        long IDataRecord.GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
-        char IDataRecord.GetChar(int i) => throw new NotImplementedException();
-        long IDataRecord.GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
-        Guid IDataRecord.GetGuid(int i) => throw new NotImplementedException();
-        short IDataRecord.GetInt16(int i) => throw new NotImplementedException();
-        int IDataRecord.GetInt32(int i) => throw new NotImplementedException();
-        long IDataRecord.GetInt64(int i) => throw new NotImplementedException();
-        float IDataRecord.GetFloat(int i) => throw new NotImplementedException();
-        double IDataRecord.GetDouble(int i) => throw new NotImplementedException();
-        string IDataRecord.GetString(int i) => throw new NotImplementedException();
-        decimal IDataRecord.GetDecimal(int i) => throw new NotImplementedException();
-        DateTime IDataRecord.GetDateTime(int i) => throw new NotImplementedException();
-        IDataReader IDataRecord.GetData(int i) => throw new NotImplementedException();
-        bool IDataRecord.IsDBNull(int i) => throw new NotImplementedException();
-        object IDataRecord.this[int i] => throw new NotImplementedException();
-        object IDataRecord.this[string name] => throw new NotImplementedException();
-        void IDataReader.Close() => throw new NotImplementedException();
-        DataTable IDataReader.GetSchemaTable() => throw new NotImplementedException();
-        bool IDataReader.NextResult() => throw new NotImplementedException();
-        int IDataReader.Depth => throw new NotImplementedException();
-        bool IDataReader.IsClosed => throw new NotImplementedException();
-        int IDataReader.RecordsAffected => throw new NotImplementedException();
-        void IDisposable.Dispose() { }
+        [ExcludeFromCodeCoverage] string IDataRecord.GetName(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] string IDataRecord.GetDataTypeName(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] Type IDataRecord.GetFieldType(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] int IDataRecord.GetValues(object[] values) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] bool IDataRecord.GetBoolean(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] byte IDataRecord.GetByte(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] long IDataRecord.GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] char IDataRecord.GetChar(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] long IDataRecord.GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] Guid IDataRecord.GetGuid(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] short IDataRecord.GetInt16(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] int IDataRecord.GetInt32(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] long IDataRecord.GetInt64(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] float IDataRecord.GetFloat(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] double IDataRecord.GetDouble(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] string IDataRecord.GetString(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] decimal IDataRecord.GetDecimal(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] DateTime IDataRecord.GetDateTime(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] IDataReader IDataRecord.GetData(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] bool IDataRecord.IsDBNull(int i) => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] object IDataRecord.this[int i] => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] object IDataRecord.this[string name] => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] void IDataReader.Close() => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] DataTable IDataReader.GetSchemaTable() => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] bool IDataReader.NextResult() => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] int IDataReader.Depth => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] bool IDataReader.IsClosed => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] int IDataReader.RecordsAffected => throw new NotImplementedException();
+        [ExcludeFromCodeCoverage] void IDisposable.Dispose() { }
 
         #endregion
     }
